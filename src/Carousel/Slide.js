678be7c9ab0a2810712f21209304001af93ae254
FarjanaFateh <farjana.fateh@escalesolutions.com>
2023-03-07 09:41:46 +0530

    Carousel Add

---
+++ src/Carousel/Slide.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const slides = [
+   " https://i.ibb.co/ncrXc2V/1.png",
+"https://i.ibb.co/B3s7v4h/2.png",
+"https://i.ibb.co/XXR8kzF/3.png",
+"https://i.ibb.co/yg7BSdM/4.png"
+
+]
+
+
+
+export default function Slide() {
+    return (
+        <div className='App'>
+            <div className='container'>
+<Crousel>
+    
+</Crousel>
+            </div>
+        </div>
+    )
+}
+
+
+
+
+
